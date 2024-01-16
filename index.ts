ada4ee8b0f42974c5ad3fcab3941a83b962a5c48
Leviathenn <levixanderson@hotmail>
2024-01-15 19:58:30 -0500

    ji

--- index.ts
+++ index.ts
@@ -1,1 +1,23 @@
-console.log("Hello via Bun!");
+/**
+ * @author Leviathan
+ */
+
+const commandLineArguments = [];
+const ignoreArgs = [process.argv[0], process.argv[1]];
+function Usage(){
+    return `
+        ./PSLX | PSLX <option> <url | mergefile> 
+    `
+}
+process.argv.forEach(arg => {
+    if(!ignoreArgs.includes(arg)){
+        commandLineArguments.push(arg)
+    }else{
+        
+    }
+});
+
+if(commandLineArguments.length == 0){
+    console.log(Usage());
+    process.exit(1);
+}
